b8b563955058b5eae4baf54e9f6b17c61c405747
liz <lizzyapowell@gmail.com>
2019-11-21 11:20:12 -0700

    updated pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,70 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="css/style.css" rel="stylesheet">
-    <title>Elizabeth's Personal Portfolio</title>
-</head>
-<body>
-    <nav id="navbar">
-<ul id="nav-list">
-    <li><a href="#welcome-section">About</a></li>
-    <li><a href="#projects">Work</a></li>
-    <li><a href="#contact">Contact</a></li>
-</ul>
-    </nav>
-
-    <section id="welcome-section" class="welcome-section">
-        <img class="personal-pic" src="img/bio.JPG">
-        <h1>Hi I am <span class="extra-bold">Elizabeth</span></h1>
-        <p>a web developer</p>
-      </section>
-
-    <section id="projects" class="projects-section">
-        <h2 class="underline-header">These are some of my projects...</h2>
-
-    <div class="project-grid">
-        
-            <a href="http://www.destinationnursery.com/" target="_blank" class="project-tile">
-                <img class="project-image" src="img/destinationnursery.jpg">
-                <p class="project-title">Destination Nursery</p></a>
-            <a href="http://www.andreakirkart.com/" target="_blank" class="project-tile">
-                <img class="project-image" src="img/andreakirkart.jpg">
-                <p class="project-title">Andrea Kirk Art</p></a>
-            <a href="starwars.html" target="_blank" class="project-tile">
-                    <img class="project-image" src="img/age-of-star-wars-1537223456465_1280w.jpg">
-                    <p class="project-title">Star Wars Characters</p></a>    
-            <a href="starships.html" target="_blank" class="project-tile">
-                    <img class="project-image" src="http://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/article_width/public/2016/01/outrider.jpg?itok=NnKAPCk4">
-                    <p class="project-title">Star Wars Star Ships</p></a>  
-            <a href="pokemon.html" target="_blank" class="project-tile">
-                    <img class="project-image" src="https://s-media-cache-ak0.pinimg.com/originals/1d/71/58/1d715856826875b49d28beaf50f5b98e.jpg">
-                    <p class="project-title">Pokemon Cards</p></a> 
-            <a href="pokemon.html" target="_blank" class="project-tile">
-                    <img class="project-image" src="https://www.militaryplaques.com/images/com_hikashop/upload/united_states_senate_seal.png">
-                    <p class="project-title">United States Senators</p></a>         
-    </div>
-
-    </section>  
-    <section id="contact" class="contact-section">
-        <div class="contact-section-header">
-            <h3>Lets work together...</h3>
-        <p> Contact me by clicking on a link below</p></div>
-        <div id="contact-link">
-            <a id="profile-link" href="https://www.instagram.com/lifeandlizzie/" target="_blank" class="contact-details"><i class="fa fa-instagram"> instagram</i></a>
-            <a id="profile-link" href="https://github.com/eapowell" target="_blank" class="contact-details"><i class="fa fa-git-square"> github</i></a>
-            
-            <a id="profile-link" href="https://www.facebook.com/elizabeth.powell.3760" target="_blank" class="contact-details"><i class="fa fa-facebook-official"> facebook</i></a>
-            <a id="profile-link" href="mailto:lizzyapowell@gmail.com" class="contact-details"><i class="fa fa-envelope"> email</i></a>
-        </div>
-            <footer>
-                <p>This portfolio is being used for educational purposes</p>
-        <p>   © 2019 Created by Elizabeth Powell</p>
-    </footer>
 
 
-</body>
-</html>
+// reusable asyne function to fetch data from url api
+async function getAPIData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+  } catch (error) {
+    console.error(error);
+  }
+  }
+  
+  // now, use the returned async data    
+  const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/&limit=25').then(data => {
+      for (const pokemon of data.results) {
+          getAPIData(pokemon.url).then(pokedata => {
+            populateDOM(pokedata);
+          //   populateDOM(Thoremon)
+          });      
+      }
+  });
+  
+  
+
+
+let mainArea = document.querySelector("main");
+addButton();
+
+//Function to append card elements into Main
+function populateDOM(single_pokemon) {
+  let pokeScene = document.createElement("div");
+  let pokeDiv = document.createElement("div");
+  let pokeFront = document.createElement("div");
+  let pokeBack = document.createElement("div");
+  let name = document.createElement("h2");
+  let pic = document.createElement("img");
+  let powers = document.createElement("p");
+  let height = document.createElement("p");
+  let weight = document.createElement("p");
+  let pokeId = document.createElement("p");
+  let forms = document.createElement("p");
+  let hr = document.createElement("hr");
+  let tipes = document.createElement("div");
+
+  pokeScene.setAttribute("class", "scene");
+  tipes.setAttribute("class", "pre");
+  pokeDiv.setAttribute("class", "card");
+  pokeFront.setAttribute("class", "card__face card__face--front");
+  pokeBack.setAttribute("class", "card__face card__face--back");
+  pic.setAttribute("class", "picDivs");
+
+  let pokeNum = getPokeNumber(single_pokemon.id);
+
+  name.textContent = capitalize(`${single_pokemon.name}`);
+  height.textContent = `Height: ${single_pokemon.height}`;
+  // powers.textContent = `Abilities: ${capitalize(
+  //   single_pokemon.abilities[0].ability.name
+  // )}`;
+
+  weight.textContent = `Weight: ${single_pokemon.weight}`;
+  pokeId.textContent = `ID: ${single_pokemon.id}`;
+  forms.textContent = `Forms: ${capitalize(single_pokemon.forms[0].name)}`;
+
+  pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeNum}.png`;
+
+  pokeFront.appendChild(pic);
+  pokeFront.appendChild(name);
+  pokeFront.appendChild(forms);
+  pokeFront.appendChild(pokeId);
+  pokeFront.appendChild(hr);
+
+  pokeBack.appendChild(powers);
+  pokeBack.appendChild(height);
+  pokeBack.appendChild(weight);
+  pokeBack.appendChild(tipes);
+
+  pokeDiv.appendChild(pokeFront);
+  pokeDiv.appendChild(pokeBack);
+  pokeScene.appendChild(pokeDiv);
+
+  mainArea.appendChild(pokeScene);
+
+  pokeDiv.addEventListener("click", function() {
+    pokeDiv.classList.toggle("is-flipped");
+  });
